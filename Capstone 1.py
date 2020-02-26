867c824f17abcf5ef8d65458fe6233223ccf23f2
vannguyen92 <61125788+vannguyen92@users.noreply.github.com>
2020-02-25 23:21:14 -0500

    Add files via upload

---
+++ Capstone 1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project
+
+# In[10]:
+
+
+import numpy as np
+
+
+# In[11]:
+
+
+import pandas as pd
+
+
+# In[13]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
